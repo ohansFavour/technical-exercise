4b6ecda1c2efad27b6e8d04bd5604c89b1a1a1b4
Ben Robbins <ben.robbins@gmail.com>
2020-09-15 15:30:32 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//This project uses create-react-app. Use `npm start` to start the local development server. (see https://create-react-app.dev/docs/getting-started/#scripts for more details)
+
+//Please create and use a component (or components) which displays a list of example data.
+//Fetch the data using a network request rather than an import statement.
+//example data lives at http://localhost:3000/example_data.json
+//each item should display a name, description, date
+//the items should be sorted by name before being displayed
+
 function App() {
   return (
     <div className="App">
